0b59f0c1f5c22c311e2899f2f8b2baa8f6575b09
Rik Hennekens <r.hennekens@student.fontys.nl>
2023-02-09 15:08:20 +0100

    kentekencheck

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -9,9 +12,6 @@ var CurrentTime int = 0
 
 func TimeCheck() {
 	CurrentTime := time.Now().Format("15")
-
-	//fmt.Println(CurrentTime)
-
 	if CurrentTime >= "7" && CurrentTime < "12" {
 		fmt.Println("Goede morgen!")
 	} else if CurrentTime > "12" && CurrentTime < "18" {
@@ -23,6 +23,36 @@ func TimeCheck() {
 	}
 }
 
+func KentekenCheck() {
+	// Lijst met toegestane kentekens
+	allowedLicenses := []string{"AB-123-CD", "EF-456-GH", "IJ-789-KL"}
+
+	// Vraag het kenteken aan de gebruiker
+	fmt.Print("Voer het kenteken in: ")
+	reader := bufio.NewReader(os.Stdin)
+	license, _ := reader.ReadString('\n')
+	license = strings.TrimSpace(license)
+
+	// Controleer of het kenteken aanwezig is in de lijst met toegestane kentekens
+	found := false
+	for _, l := range allowedLicenses {
+		if l == license {
+			found = true
+			break
+		}
+	}
+
+	// Toon het juiste bericht op basis van of het kenteken aanwezig is of niet
+	if found {
+		fmt.Println("Welkom op het parkeerterrein.")
+		//TimeCheck staat hier vanwege het moment dat "Goedendag" wordt aangeroepen. Dit moet pas gebeuren als het kenteken is gecontroleerd.
+		TimeCheck()
+	} else {
+		fmt.Println("Je hebt helaas geen toegang tot het parkeerterrein.")
+	}
+}
+
 func main() {
-	TimeCheck()
+	KentekenCheck()
+	//TimeCheck staat in de KentekenCheck omdat het moment dat "Goedendag" wordt aangeroepen afhankelijk is van het kenteken.
 }
