6b08e0aae1f3adcdaef3e73d9fc9180c5dd03b68
Rik Hennekens <r.hennekens@student.fontys.nl>
2023-02-09 14:43:09 +0100

    Goede morgen

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var CurrentTime int = 0
+
+func TimeCheck() {
+	CurrentTime := time.Now().Format("15")
+
+	//fmt.Println(CurrentTime)
+
+	if CurrentTime >= "7" && CurrentTime < "12" {
+		fmt.Println("Goede morgen!")
+	} else if CurrentTime > "12" && CurrentTime < "18" {
+		fmt.Println("Goede middag!")
+	} else if CurrentTime > "18" && CurrentTime < "23" {
+		fmt.Println("Goede avond!")
+	} else if CurrentTime > "23" && CurrentTime < "7" {
+		fmt.Println("Sorry, de parkeerplaats is snachts gesloten")
+	}
+}
 
 func main() {
-	fmt.Println("Welkom bij Fonteyn Vakantieparken!")
+	TimeCheck()
 }
